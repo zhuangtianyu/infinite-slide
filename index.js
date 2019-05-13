07405b3160a27208c94cd383cb930f5fb59fc08e
Zhuang TianYu <zhuangtianyu@weimingedu.com>
2019-05-13 14:42:43 +0800

    setTimeout.delay = 4ms, 优化代码

--- index.js
+++ index.js
@@ -1,8 +1,23 @@
 const state = {
+  left: 0,
   index: 0,
   length: 0,
   moving: false
 }
+
+const config = {
+  frequency: void 0,
+  duration: void 0
+}
+
+const element = {
+  slide: void 0,
+  container: void 0,
+  prev: void 0,
+  next: void 0
+}
+
+const style = { width: 0, height: 0 }
 
 const append = (parent, element, klass, style = {}) => {
   const child = document.createElement(element)
@@ -12,26 +27,33 @@ const append = (parent, element, klass, style = {}) => {
   return child
 }
 
-const mount = (slide, data, height) => {
-  // 获取 slide clientWidth
-  const { clientWidth } = slide
-  // 挂载 width 到 state
-  Object.assign(state, { width: clientWidth })
+const mount = (data) => {
+  const { slide } = element
+  const { clientWidth: width } = slide
+  const { height } = style
+  // 挂载 width 到 style
+  Object.assign(style, { width })
 
   // 挂载 prev, next
-  append(slide, 'span', 'prev').innerHTML = 'prev'
-  append(slide, 'span', 'next').innerHTML = 'next'
+  const prev = append(slide, 'span', 'prev')
+  const next = append(slide, 'span', 'next')
+  prev.innerHTML = 'prev'
+  next.innerHTML = 'next'
+  Object.assign(element, { prev, next })
 
   // 挂载 slide-container
-  const width = `${clientWidth * (data.length + 1)}px`
-  const container = append(slide, 'div', 'slide-container', { width })
-  Object.assign(state, { container })
+  const containerStyle = {
+    width: `${width * (data.length + 1)}px`,
+    height: `${style.height}px`
+  }
+  const container = append(slide, 'div', 'slide-container', containerStyle)
+  Object.assign(element, { container })
 
   // 挂载 slide-item
   data.concat(data[0]).map(url => {
     const style = {
       'background-image': `url(${url})`,
-      'width': `${clientWidth}px`,
+      'width': `${width}px`,
       'height': `${height}px`
     }
     append(container, 'div', 'slide-item', style)
@@ -41,48 +63,53 @@ const mount = (slide, data, height) => {
   Object.assign(state, { length })
 }
 
-const calculate = {
-  left: () => {
-    return 0
-  },
-  speed: () => {
-    return 0
-  }
-}
-
 const get = {
-  left: () => -state.index * state.width
+  left: () => - state.index * style.width
 }
 
 const set = {
   left: (step) => {
-    const { index, length, container, width } = state
-    if (index + step > length) {
-      Object.assign(state, { index: 0 })
-    }
-    if (index + step < 0) {
-      Object.assign(state, { index: length })
-    }
-    Object.assign(container.style, { left: `${-state.index * width}px` })
+    const { index, length } = state
+    const { container } = element
+    const { width } = style
+    index + step > length
+      ? Object.assign(state, { index: 0 })
+      : void 0
+    index + step < 0
+      ? Object.assign(state, { index: length })
+      : void 0
+    const left = `${- state.index * width}px`
+    Object.assign(container.style, { left })
+  }
+}
+
+const calculate = (step) => {
+  const { index } = state
+  const { width } = style
+  const { duration } = config
+  // speed * 4 => 修正 browser setTimeout delay min 4ms
+  return {
+    target: get.left() - step * width,
+    speed: Math.round(- step * width / duration) * 4
   }
 }
 
 const move = (target, speed, step) => {
   console.time('move')
   Object.assign(state, { moving: true })
-  const { index, container } = state
-  // fake interval start
+  const { index } = state
+  const { container } = element
+  // virtual interval start
   const interval = () => setTimeout(() => {
     const { offsetLeft } = container
     if (Math.abs(target - offsetLeft) < Math.abs(speed)) {
       Object.assign(container.style, { left: `${target}px` })
       Object.assign(state, { index: index + step, moving: false })
-      // fake interval end
       return console.timeEnd('move')
     }
     Object.assign(container.style, { left: `${offsetLeft + speed}px` })
     interval()
-  }, 1)
+  }, 4)
   interval()
 }
 
@@ -90,16 +117,13 @@ const trigger = (step) => {
   if (state.moving) { return }
   // 修正 container left
   set.left(step)
-  // calculate target && speed => move(target, speed, step)
-  const { width, duration, container, index } = state
-  const target = get.left() - step * width
-  const speed = Math.round(-step * width / duration)
+  // calculate target && speed
+  const { target, speed } = calculate(step)
   move(target, speed, step)
 }
 
 const declare = () => {
-  const prev = document.querySelector('.prev')
-  const next = document.querySelector('.next')
+  const { prev, next } = element
   prev.onclick = () => trigger(-1)
   next.onclick = () => trigger(1)
 }
@@ -122,20 +146,24 @@ const slide = ({
   height = 200
 }) => {
   const slide = document.querySelector(selector)
-  // position: absulote 的 slide-item 无法撑起 height
+  // assign slide to element
+  Object.assign(element, { slide })
+  // position: absulote 的 slide-container 无法撑起 height
   Object.assign(slide.style, { height: `${height}px` })
-
-  // 挂载 slide, frequency, duration 到 state
-  Object.assign(state, { slide, frequency, duration })
+  // assign height to style
+  Object.assign(style, { height })
+  // assign config to config
+  Object.assign(config, { frequency, duration })
 
   // get slide array classlist
   const classList = Array.from(slide.classList)
-  if (classList.indexOf('slide') === -1) {
-    slide.classList.add('slide')
-  }
+  // add slide class to slide element
+  classList.indexOf('slide') === -1
+    ? slide.classList.add('slide')
+    : void 0
 
-  // mount dom
-  mount(slide, data, height)
+  // mount container + item + prev + next
+  mount(data)
 
   // declare event
   declare()
